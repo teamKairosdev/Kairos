ec2656716bbaa8d26186f7dfa4874dc3f0179a1f
Rheehose <sea35652@gmail.com>
2026-07-26 21:18:46 +0900

    chore: remove build artifacts from git tracking and add .gitignore
    .output, node_modules 빌드 결과물을 git 추적 대상에서 제거

--- .output/server/chunks/build/styles.mjs
+++
@@ -1,8 +0,0 @@
-const interopDefault = r => r.default || r || [];
-const styles = {
-  "../node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.DjEXIT5C.mjs').then(interopDefault),
-  "../node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.DObwArFs.mjs').then(interopDefault)
-};
-
-export { styles as default };
-//# sourceMappingURL=styles.mjs.map
